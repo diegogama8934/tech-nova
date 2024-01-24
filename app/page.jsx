27c91235bfa0e8263222d963ef69daa5d2361de9
Alan J Morales <amorales130@alumnos.uaq.mx>
2024-01-24 12:48:55 -0600

    Profile Card

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LandingPage from "@/Components/Screens/General/LandingPage/LandingPage";
+import ProfileCard from "@/Components/Atoms/ProfileCard/ProfileCard";
 
 export default function Home() {
   return (
     <>
       <LandingPage />
+      {/* <ProfileCard /> */}
     </>
   )
 }
